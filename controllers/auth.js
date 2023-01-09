6d21ede28158d670bc22fcbf84a1d7eecb1b7316
Jeff Phan <jeffphan8816@gmail.com>
2023-01-09 11:04:26 +1100

    fixed token const to let

--- controllers/auth.js
+++ controllers/auth.js
@@ -70,11 +70,12 @@ export const login = async (req, res) => {
         }
         const token = jwt.sign({ email: user.email, id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
         delete user.password;
-        res.status(200).json({ result: user, token });
+        res.status(200).json({ user, token });
     } catch (error) {
+      
+      console.log("login error")
+      console.log(error);
         res.status(500).json({ message: "Something went wrong" });
-        console.log("login error")
-        console.log(error);
     }
 }
 
--- middleware/auth.js
+++ middleware/auth.js
@@ -3,16 +3,21 @@ import User from "../models/User.js";
 
 export const verifyToken = async (req, res, next) => {
   try {
-    const token = req.header("Authorization");
-    if (!token) return res.status(403).send("Invalid Authentication");
+    let token = req.header("Authorization");
+
+    if (!token) {
+      return res.status(403).send("Invalid Authentication");
+    }
+
     if (token.startsWith("Bearer ")) {
       token = token.slice(7, token.length).trimLeft();
     }
+
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     req.user = verified;
     next();
   } catch (err) {
-    return res.status(401).json("You are not authenticated!");
+    // console.log(err)
+    res.status(401).json("You are not authenticated!");
   }
-
 };
